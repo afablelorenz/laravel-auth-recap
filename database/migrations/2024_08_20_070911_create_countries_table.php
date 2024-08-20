863919c1f23de9f3369e7651ce6a62a344a16250
afablelorenz <afablelorenz@gmail.com>
2024-08-20 09:18:07 +0200

    created models

---
+++ database/migrations/2024_08_20_070911_create_countries_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->unsignedSmallInteger("number_of_participants");
+            $table->text("flag_img");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('countries');
+    }
+};
